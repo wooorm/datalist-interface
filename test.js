b15901293c06885d5d4dea4b55774650b4867d0f
Titus Wormer <tituswormer@gmail.com>
2018-04-19 21:10:16 +0200

    Update dev-dependencies

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var test = require('tape');
-var Interface = require('./');
+var Interface = require('.');
 
 test('#is(value)', function (t) {
   var fish = new Interface(['shark', 'tuna']);
