712cb25fcd91caa612307c8c82dab9bf359dcf9f
Titus Wormer <tituswormer@gmail.com>
2019-05-09 10:16:46 +0200

    Refactor comment style

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ proto.valueOf = all
 proto.toJSON = all
 proto.toString = toString
 
-/* An interface for a list of items. */
+// An interface for a list of items.
 function DatalistInterface(values) {
   this.values = []
   this.add.apply(this, values)
 }
 
-/* Add all arguments. */
+// Add all arguments.
 function add(/* values... */) {
   var self = this
 
@@ -28,7 +28,7 @@ function add(/* values... */) {
   return self
 }
 
-/* Remove all arguments. */
+// Remove all arguments.
 function remove(/* values... */) {
   var values = this.values
   var index = arguments.length
@@ -45,17 +45,17 @@ function remove(/* values... */) {
   return this
 }
 
-/* Whether or not `value` is in context. */
+// Whether or not `value` is in context.
 function is(value) {
   return this.values.indexOf(value) !== -1
 }
 
-/* Get all values. */
+// Get all values.
 function all() {
   return this.values.concat()
 }
 
-/* Stringify all values. */
+// Stringify all values.
 function toString() {
   return this.values.toString()
 }
