9cccb649f9cadeece39682ca0be578612ab693d8
Titus Wormer <tituswormer@gmail.com>
2022-11-12 10:16:24 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,99 +1,99 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {DatalistInterface} from './index.js'
 
-test('#is(value)', function (t) {
+test('#is(value)', function () {
   const fish = new DatalistInterface(['shark', 'tuna'])
 
-  t.equal(
+  assert.equal(
     fish.is('shark'),
     true,
     'should return true if a value is in the database'
   )
 
-  t.equal(
+  assert.equal(
     fish.is('unicorn'),
     false,
     'should return false if a value is not in the database'
   )
 
-  t.equal(
+  assert.equal(
     fish.is('unicorn'),
     fish.has('unicorn'),
     '`has` should be an alias for `is`'
   )
-
-  t.end()
 })
 
-test('#toString()', function (t) {
+test('#toString()', function () {
   const fish = new DatalistInterface(['shark', 'tuna'])
 
-  t.equal(
+  assert.equal(
     fish.toString(),
     'shark,tuna',
     'should return values delimited by commas'
   )
-
-  t.end()
 })
 
-test('#all()', function (t) {
+test('#all()', async function (t) {
   const mammals = new DatalistInterface(['colugo', 'human'])
   const all = mammals.all()
 
-  t.ok(Array.isArray(all), 'should return an array')
+  assert.ok(Array.isArray(all), 'should return an array')
 
-  t.deepEqual(all, ['colugo', 'human'], 'should return all values in context')
+  assert.deepEqual(
+    all,
+    ['colugo', 'human'],
+    'should return all values in context'
+  )
 
-  t.test('should be immutable', function (st) {
+  await t.test('should be immutable', function () {
     all.push('unicorn')
 
-    st.equal(mammals.is('unicorn'), false)
-    st.equal(mammals.all().indexOf('unicorn'), -1)
-    st.end()
+    assert.equal(mammals.is('unicorn'), false)
+    assert.equal(mammals.all().indexOf('unicorn'), -1)
   })
 
-  t.deepEqual(
+  assert.deepEqual(
     mammals.all(),
     mammals.valueOf(),
     '`valueOf` should be an alias for `all`'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     mammals.all(),
     mammals.toJSON(),
     '`toJSON` should be an alias for `all`'
   )
-
-  t.end()
 })
 
-test('#add() and #remove()', function (t) {
+test('#add() and #remove()', function () {
   const mammals = new DatalistInterface(['colugo', 'human'])
 
-  t.equal(mammals.add('unicorn'), mammals, '`add` should return self')
-  t.equal(mammals.is('unicorn'), true, 'should add values')
+  assert.equal(mammals.add('unicorn'), mammals, '`add` should return self')
+  assert.equal(mammals.is('unicorn'), true, 'should add values')
 
-  t.equal(mammals.remove('unicorn'), mammals, '`remove` should return self')
-  t.equal(mammals.is('unicorn'), false, 'should remove values')
+  assert.equal(
+    mammals.remove('unicorn'),
+    mammals,
+    '`remove` should return self'
+  )
+  assert.equal(mammals.is('unicorn'), false, 'should remove values')
 
-  t.deepEqual(
+  assert.deepEqual(
     mammals.add('unicorn', 'doge').all(),
     ['colugo', 'human', 'unicorn', 'doge'],
     'should add all arguments'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     mammals.remove('unicorn', 'doge').all(),
     ['colugo', 'human'],
     'should remove all arguments'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     mammals.remove('unicorn').all(),
     ['colugo', 'human'],
     'should ignore removing non-existing `value`'
   )
-
-  t.end()
 })
